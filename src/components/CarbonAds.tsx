8d50501c64c10e0e9905b594f65d0597c212d7b1
Artem Zakharchenko <kettanaito@gmail.com>
2020-10-29 18:49:06 +0100

    Adds Carbon ads to the documentation

---
+++ src/components/CarbonAds.tsx
@@ -0,0 +1,12 @@
+import './carbon-ads.css'
+
+export const CarbonAds: React.FC = () => {
+  return (
+    <script
+      async
+      type="text/javascript"
+      src="//cdn.carbonads.com/carbon.js?serve=CEBIV5QY&placement=mswjsio"
+      id="_carbonads_js"
+    />
+  )
+}
